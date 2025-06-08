f6b383a9a0a21970e8ef99b266a02140e927cd48
w4d32001 <freedom01022001@gmail.com>
2025-06-07 20:27:55 -0500

    first commit

---
+++ src/const/navconst.js
@@ -0,0 +1,12 @@
+export const NAV_DATA = [
+    { href: "/", label: "Inicio" },
+    { href: "/#history", label: "Historia" },
+    { href: "/#us", label: "Nosotros" },
+    { href: "/#curriculum", label: "Plan de estudios" },
+    { href: "/#teachers", label: "Docentes" },
+    { href: "/#news", label: "Noticias" },
+    { href: "/#gallery", label: "Galería" },
+    { href: "/#applications", label: "Aplicaciones" },
+    { href: "/#research", label: "Investigaciones" },
+    { href: "/#contact", label: "Contacto" }
+];
